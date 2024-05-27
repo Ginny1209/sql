fb163edaa3259492d78586131d3739d85339f22f
Ginny1209 <61629844+Ginny1209@users.noreply.github.com>
2024-05-27 15:15:19 -0400

    Update homework_4.sql
    restore the main branch without homework

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -18,10 +18,6 @@ Edit the appropriate columns -- you're making two edits -- and the NULL rows wil
 All the other rows will remain the same.) */
 
 
-SELECT 
-    product_name || ', ' || COALESCE(product_size, '') || ' (' || COALESCE(product_qty_type, 'unit') || ')'
-FROM product;
-
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -33,97 +29,12 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
-SELECT 
-    customer_id,
-    market_date,
-    ROW_NUMBER() OVER (PARTITION BY customer_id, market_date ORDER BY market_date) AS visit_number
-FROM 
-    customer_purchases;
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
-SELECT 
-    customer_id,
-    market_date
-FROM 
-    (
-        SELECT 
-            customer_id,
-            market_date,
-            ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS reversed_visit_number
-        FROM 
-            customer_purchases
-    ) AS Subquery
-WHERE 
-    reversed_visit_number = 1;
 
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
-SELECT 
-    customer_id,
-    product_id,
-    market_date,
-    COUNT(*) OVER (PARTITION BY customer_id, product_id) AS purchase_count
-FROM 
-    customer_purchases;
 
-
-/* STRING*/
-
-SELECT 
-    product_name,
-    CASE 
-        WHEN INSTR(product_name, '-') > 0 THEN TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1))
-        ELSE NULL 
-    END AS description
-FROM 
-    product;
-
-/* UNION*/
-
-WITH TotalSales AS (
-    SELECT 
-        market_date,
-        SUM(quantity * cost_to_customer_per_qty) AS total_sales
-    FROM 
-        customer_purchases
-    GROUP BY 
-        market_date
-),
-BestDay AS (
-    SELECT 
-        market_date,
-        total_sales
-    FROM 
-        TotalSales
-    ORDER BY 
-        total_sales DESC
-    LIMIT 1
-),
-WorstDay AS (
-    SELECT 
-        market_date,
-        total_sales
-    FROM 
-        TotalSales
-    ORDER BY 
-        total_sales ASC
-    LIMIT 1
-)
-SELECT 
-    market_date,
-    total_sales,
-    'Best Day' AS day_type
-FROM 
-    BestDay
-
-UNION
-
-SELECT 
-    market_date,
-    total_sales,
-    'Worst Day' AS day_type
-FROM 
-    WorstDay;
